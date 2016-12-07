2352c72fdf24c4c9131f1171b9014fcfd89e4944
Tong Lei <saigut@qq.com>
2016-12-07 12:59:41 +0800

    init with spell and srfi libraries

---
+++ README.md
@@ -0,0 +1,3 @@
+# Personal collections of Chez Scheme libraries
+
+You can put .chezscheme_libs directly under $HOME. There is a script `setup_libs.sh` for setting CHEZSCHEMELIBDIRS.
---
+++ setup_libs.sh
@@ -0,0 +1,8 @@
+#！/bin/bash
+
+echo '' >> ~/.bashrc
+echo 'CHEZSCHEMELIBDIRS="~/.chezscheme_libs:"' >> ~/.bashrc
+
+## fish shell config
+# echo '' >> ~/.config/fish/config.fish
+# echo 'set -x CHEZSCHEMELIBDIRS "~/.chezscheme_libs:"' >> ~/.config/fish/config.fish
